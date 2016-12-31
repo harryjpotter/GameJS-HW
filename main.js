160bca61bf39f2c1bd58eb9f8698b06367ed3ab1
harryjpotter <rotem.marinov@gmail.com>
2016-12-31 15:11:52 +0200

    I finished the game

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 
 var secretNum = Math.floor((Math.random() * 100) + 1);
+var counter = 0;
 
-$(document).ready(function(){
-   alert("The computer chose a number between 1 and 100. You need to guess it. Good-luck! :) ") 
+$(document).ready(function () {
+    alert("The computer chose a number between 1 and 100. You need to guess it. Good-luck! :) ")
 });
 
 function give_up() {
-    alert("the number is:" + x);
+    alert("the number was: " + secretNum + "\n" + "It took you " + counter + " guesses to give up");
+    restart();
 }
 
 function guessing() {
-
-
     var GuessedNum = $("#guessingArea").val();//המספר שמנחשים
-    if(GuessedNum>secretNum)
+    if (GuessedNum > secretNum)
         alert("LOWER!");
-    if(GuessedNum<secretNum)
+    if (GuessedNum < secretNum)
         alert("HIGHER!");
-    if(secretNum==GuessedNum)
-    alert("YES, THIS IS THE NUMBER!");
+    if (secretNum == GuessedNum)
+        alert("YES, THIS IS THE NUMBER!" + "\n" + "It took you " + counter + " guesses to win");
+    counter++;
+}
+
+function restart() {
+    secretNum = Math.floor((Math.random() * 100) + 1);
+    alert("The computer has chosen another number, now you can play again")
 }
